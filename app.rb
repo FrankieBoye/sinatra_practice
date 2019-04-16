214cf686412b49186725f5c0a61d22447c4e1d26
FrankieBoye <frankie4@btinternet.com>
2019-04-16 09:40:54 +0100

    returning HTML

--- app.rb
+++ app.rb
@@ -5,7 +5,12 @@ get '/' do
   "Hello World"
 end
 
-
 get '/secret' do
-  "Lewis Hamilton Formula 1 2019 champion!!"
+  "This is a secret message."
+end
+
+get '/cat' do
+  "<div style='border: 4px red dotted'>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
